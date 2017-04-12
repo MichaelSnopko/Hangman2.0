172f04b55f9e0105a8908041876d29384f63f7ef
Michael Snopko <michael.snopko@gmail.com>
2017-04-12 10:00:18 -0400

    Added Index and javaScript. Currently don't use database.

---
+++ hangman/web/script.js
@@ -0,0 +1,502 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var rand = 0;
+var word = "";
+var numWrong = 0;
+var numRight = 0;
+var wordLength = 0;
+var numChar = 0;
+var tempWords = ["Early bird gets the worm", "Read between the lines", "I can eat a horse", "Twenty-four seven", "Cat got your tounge", "One in one million", "I beg to differ", "Easier said than done", "Add insult to injury", "Don't cry over spilled milk", "Curiosity killed the cat", "Two peas in a pod", "That's the last straw", "Piece of cake", "Speak of the devil", "Go the whole nine yards", "An eye for an eye", "Hit the hay", "Stab someone in the back", "Quit cold turkey", "Cut to the chase", "Best of both worlds", "Kill two birds with one stone", "Break a leg", "Hit the nail on the head", "Kick the bucket"];     
+
+//$.getJSON('/words').done(function(data))
+
+function Game(){
+    document.getElementById('introPage').style.display = "none";
+    document.getElementById('singlePage').style.display = "block";
+}
+
+
+function Words(){
+    rand = Math.floor(Math.random()*tempWords.length);
+    word = tempWords[rand];
+    document.getElementById('singlePage').style.display = "none";
+    document.getElementById('categoryName').innerHTML = "Words";
+    hangman();
+}
+
+    if(numRight==1){
+        results.style.visibility = "visible";
+        results.style.color = "red";
+        results.innerHTML = "You lose!";        
+        if(document.getElementById('underline1').offsetWidth == 50){
+            results.style.fontSize = "200px";
+            results.style.height = "200px";
+            results.style.lineHeight = "200px";
+        }
+        if(document.getElementById('underline1').offsetWidth == 28){
+            results.style.marginTop = "20px";
+            results.style.fontSize = "100px";
+            results.style.height = "100px";
+            results.style.lineHeight = "100px";
+        }
+        if(document.getElementById('underline1').offsetWidth == 18){
+            results.style.marginTop = "15px";
+            results.style.fontSize = "75px";
+            results.style.height = "75px";
+            results.style.lineHeight = "75px";
+        }
+        document.getElementById('challengeBank').style.display = "none";
+        document.getElementById('again').style.display = "block";
+        document.getElementById('home').style.display = "block";
+    }
+    if(numWrong==25){
+        results.style.visibility = "visible";
+        results.style.color = "#00b100";
+        results.innerHTML = "You win!";        
+        if(document.getElementById('underline1').offsetWidth == 50){
+            results.style.fontSize = "200px";
+            results.style.height = "200px";
+            results.style.lineHeight = "200px";
+        }
+        if(document.getElementById('underline1').offsetWidth == 28){
+            results.style.marginTop = "20px";
+            results.style.fontSize = "100px";
+            results.style.height = "100px";
+            results.style.lineHeight = "100px";
+        }
+        if(document.getElementById('underline1').offsetWidth == 18){
+            results.style.marginTop = "15px";
+            results.style.fontSize = "75px";
+            results.style.height = "75px";
+            results.style.lineHeight = "75px";
+        }
+        document.getElementById('challengeBank').style.display = "none";
+        document.getElementById('again').style.display = "block";
+        document.getElementById('home').style.display = "block";
+        document.getElementById('letter1').style.visibility = "visible";
+    }
+
+
+function countChars(countfrom,displayto) {
+    var len = document.getElementById(countfrom).value.length;
+    document.getElementById(displayto).innerHTML = len;
+}
+
+function readText(){
+    word = document.getElementById('input').value;
+    hangman();
+}
+
+function hangman(){
+    var x = word.length;
+    var y = x-1;
+    var spaces = 0;
+    var validChar = new Array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", " ", "?", "!", ",", ".", "-", "'");
+    for(z = 0; z < word.length; z++){
+        var letter = word.substring(y,x);
+        if(validChar.indexOf(letter) > -1){
+            x--;
+            y--;
+        }
+    }
+    x = word.length;
+    y = x-1;
+    while (x>0){
+        numChar++;
+        var letter = word.substring(y,x);
+        if(letter === " "){
+            document.getElementById('letter'+x).innerHTML = "&nbsp;";
+            document.getElementById('letter'+x).style.visibility = "hidden";
+            document.getElementById('letter'+x).style.display = "block";
+            document.getElementById('underline'+x).style.display = "block";
+            spaces++;
+        }
+        else if(letter === "?" || letter === "!" || letter === "," || letter === "." || letter === "-" || letter === "'"){
+            document.getElementById('letter'+x).innerHTML = letter;
+            document.getElementById('letter'+x).style.display = "block";
+            document.getElementById('underline'+x).style.display = "block";
+            spaces++;
+        }
+        else{
+            document.getElementById('letter'+x).innerHTML = letter;
+            document.getElementById('letter'+x).style.visibility = "hidden";
+            document.getElementById('underline'+x).style.display = "block";            
+            document.getElementById('underline'+x).style.borderBottom = "3px solid black";
+        }
+        x--;
+        y--;
+    }
+    wordLength = word.length - spaces;
+    document.getElementById('gamePage').style.display = "block";
+    splitWords();
+    document.getElementById('challengeBank').style.display = "none";
+    draw();
+}
+
+function draw(){
+    var ctx = document.getElementById("hangman").getContext('2d');
+        ctx.fillStyle = "white";
+        ctx.lineWidth=3;
+        ctx.fillRect(0, 0, 300, 300);
+        ctx.beginPath(); //vertical bar
+            ctx.moveTo(50,270);
+            ctx.lineTo(50,25);
+            ctx.stroke();
+        ctx.beginPath(); //vertical bar long piece
+            ctx.moveTo(65,270);
+            ctx.lineTo(65,85);
+            ctx.stroke();
+        ctx.beginPath(); //vertical bar short piece
+            ctx.moveTo(65,64);
+            ctx.lineTo(65,40);
+            ctx.stroke();
+        ctx.beginPath(); //horizontal bar
+            ctx.moveTo(49,25);
+            ctx.lineTo(175,25);
+            ctx.stroke();
+        ctx.beginPath(); //horizontal bar short piece
+            ctx.moveTo(49,40);
+            ctx.lineTo(86,40);
+            ctx.stroke();
+        ctx.beginPath(); //horizontal bar long piece
+            ctx.moveTo(106,40);
+            ctx.lineTo(175,40);
+            ctx.stroke();
+        ctx.beginPath(); //small vertical bar
+            ctx.moveTo(173,25);
+            ctx.lineTo(173,40);
+            ctx.stroke();
+        ctx.beginPath(); //cross bar
+            ctx.moveTo(50,80);
+            ctx.lineTo(100,25);
+            ctx.stroke();
+        ctx.beginPath(); //cross bar
+            ctx.moveTo(60,90);
+            ctx.lineTo(111,35);
+            ctx.stroke();
+        ctx.beginPath(); //cross bar
+            ctx.moveTo(50,80);
+            ctx.lineTo(60,90);
+            ctx.stroke();
+        ctx.beginPath(); //cross bar
+            ctx.moveTo(100,25);
+            ctx.lineTo(111,35);
+            ctx.stroke();
+        ctx.beginPath(); //ground
+            ctx.moveTo(35,270);
+            ctx.lineTo(265,270);
+            ctx.stroke();
+        ctx.beginPath(); //noose
+            ctx.moveTo(150,40);
+            ctx.lineTo(150,80);
+            ctx.stroke();
+        }
+
+function splitWords(){
+    var placeKeep = 0;
+    var countBack = 16;
+    if(numChar > 15){
+        while(countBack > 1){
+            if(document.getElementById('letter16').innerHTML == "&nbsp;"){
+                document.getElementById('underline16').style.width = "0px";
+                document.getElementById('underline16').style.marginRight = "0px";
+            }
+            if(document.getElementById('letter'+countBack).innerHTML == "&nbsp;"){
+                document.getElementById('underline'+countBack).style.width = (document.getElementById('underline1').offsetWidth)*(16-countBack)+"px";
+                placeKeep = countBack;
+                countBack = 0;
+            }
+            countBack--;
+        }
+    }
+    for(x=0;x<8;x++){
+        countBack = 15+placeKeep;
+        if(numChar > countBack){
+            while(countBack > 1){
+                if(document.getElementById('letter'+countBack).innerHTML == "&nbsp;"){
+                    document.getElementById('underline'+countBack).style.width = (document.getElementById('underline1').offsetWidth*((16+placeKeep)-countBack))+"px";
+                    placeKeep = countBack;
+                    countBack = 0;
+                }
+                countBack--;
+            }
+        }
+    }
+    
+}
+
+function guessLetter(){
+    var correct = 0;
+    var target = event.target || event.srcElement;
+    target.style.visibility = "hidden";
+    var lower = target.id;
+    var upper = document.getElementById(lower).getAttribute('value');
+    var results = document.getElementById('results');
+    var ul1 = document.getElementById('underline1').offsetWidth;
+    for(a = 1; a < 101; a++){
+        if(document.getElementById('letter'+a).innerHTML === upper || document.getElementById('letter'+a).innerHTML === lower){
+            document.getElementById('letter'+a).style.visibility = "visible";
+            correct++;
+            numRight++;
+        }
+    }
+    if(correct==0){
+        numWrong++;
+        hang();
+    }
+    if(numWrong==6){
+        results.style.visibility = "visible";
+        results.style.color = "red";
+        results.innerHTML = "You can't miss another letter!";
+        if(ul1 == 50){
+            results.style.lineHeight = "70px";
+            results.style.fontSize = "30px";
+        }
+        if(ul1 == 28){
+            results.style.lineHeight = "50px";
+            results.style.fontSize = "25px";
+        }
+        if(ul1 == 18){
+            results.style.lineHeight = "40px";
+            results.style.fontSize = "20px";
+        }
+    }
+    if(numWrong==7){
+        results.innerHTML = "You lose!";
+        document.getElementById('again').style.display = "block";
+        document.getElementById('home').style.display = "block";
+        document.getElementById('letterBank').style.visibility = "hidden";
+        document.getElementById('letter1').style.visibility = "visible";
+        document.getElementById('letter2').style.visibility = "visible";
+        document.getElementById('letter3').style.visibility = "visible";
+        document.getElementById('letter4').style.visibility = "visible";
+        document.getElementById('letter5').style.visibility = "visible";
+        document.getElementById('letter6').style.visibility = "visible";
+        document.getElementById('letter7').style.visibility = "visible";
+        document.getElementById('letter8').style.visibility = "visible";
+        document.getElementById('letter9').style.visibility = "visible";
+        document.getElementById('letter10').style.visibility = "visible";
+        document.getElementById('letter11').style.visibility = "visible";
+        document.getElementById('letter12').style.visibility = "visible";
+        document.getElementById('letter13').style.visibility = "visible";
+        document.getElementById('letter14').style.visibility = "visible";
+        document.getElementById('letter15').style.visibility = "visible";
+        document.getElementById('letter16').style.visibility = "visible";
+        document.getElementById('letter17').style.visibility = "visible";
+        document.getElementById('letter18').style.visibility = "visible";
+        document.getElementById('letter19').style.visibility = "visible";
+        document.getElementById('letter20').style.visibility = "visible";
+        document.getElementById('letter21').style.visibility = "visible";
+        document.getElementById('letter22').style.visibility = "visible";
+        document.getElementById('letter23').style.visibility = "visible";
+        document.getElementById('letter24').style.visibility = "visible";
+        document.getElementById('letter25').style.visibility = "visible";
+        document.getElementById('letter26').style.visibility = "visible";
+    }
+}
+
+function win(){
+    var ul1 = document.getElementById('underline1').offsetWidth;
+    var again = document.getElementById('again');
+    var results = document.getElementById('results');
+        results.style.visibility = "visible";
+        results.style.color = "#00b100";
+    if(numWrong > 6){
+        document.getElementById('letterBank').style.display = "none";
+        again.style.display = "block";
+        document.getElementById('home').style.display = "block";
+        if(ul1 == 50){
+            results.style.lineHeight = "70px";
+            results.style.fontSize = "30px";
+        }
+        if(ul1 == 28){
+            results.style.lineHeight = "50px";
+            results.style.fontSize = "25px";
+        }
+        if(ul1 == 18){
+            results.style.lineHeight = "40px";
+            results.style.fontSize = "20px";
+        }
+    }
+    else{
+        results.innerHTML = "You win!";
+        document.getElementById('letterBank').style.display = "none";
+        again.style.display = "block";
+        document.getElementById('home').style.display = "block";
+        if(ul1 == 50){
+            again.style.marginTop = "75px";
+            results.style.marginTop = "75px";
+            results.style.fontSize = "200px";
+        }
+        if(ul1 == 28){
+            again.style.marginTop = "50px";
+            results.style.marginTop = "40px";
+            results.style.fontSize = "100px";
+        }
+        if(ul1 == 18){
+            again.style.marginTop = "40px";
+            results.style.marginTop = "15px";
+            results.style.fontSize = "75px";
+        }
+    }
+}
+
+function hang(){
+    var ctx = document.getElementById("hangman").getContext('2d');
+    if(numWrong==1){
+        ctx.beginPath(); //head
+            ctx.arc(150, 100, 20, 0, 2*Math.PI);
+            ctx.stroke();
+        ctx.beginPath(); //left eye
+            ctx.arc(143, 95, 3.5, 0, 2*Math.PI);
+            ctx.stroke();
+        ctx.beginPath(); //right eye
+            ctx.arc(157, 95, 3.5, 0, 2*Math.PI);
+            ctx.stroke();
+        ctx.beginPath(); //mouth
+            ctx.arc(150, 103, 9, 0, Math.PI);
+            ctx.stroke();
+    }
+    if(numWrong==2){
+        ctx.beginPath(); //body
+            ctx.moveTo(150,120);
+            ctx.lineTo(150,190);
+            ctx.stroke();
+    }
+    if(numWrong==3){
+        ctx.fillStyle = "white";
+        ctx.fillRect(138, 102, 24, 12); //cover mouth
+        ctx.beginPath(); //straight mouth
+            ctx.moveTo(140,108);
+            ctx.lineTo(160,108);
+            ctx.stroke();
+        ctx.beginPath(); //right arm
+            ctx.moveTo(150,135);
+            ctx.lineTo(180,160);
+            ctx.stroke();
+    }
+    if(numWrong==4){
+        ctx.beginPath(); //left arm
+            ctx.moveTo(150,135);
+            ctx.lineTo(120,160);
+            ctx.stroke();
+    }
+    if(numWrong==5){
+        ctx.fillRect(138, 102, 24, 12); //cover mouth
+        ctx.beginPath(); //sad mouth
+            ctx.arc(150, 112, 9, 0, Math.PI, true);
+            ctx.stroke();
+        ctx.beginPath(); //right leg
+            ctx.moveTo(149,188);
+            ctx.lineTo(180,230);
+            ctx.stroke();
+    }
+    if(numWrong==6){
+        ctx.beginPath(); //left leg
+            ctx.moveTo(151,188);
+            ctx.lineTo(120,230);
+            ctx.stroke();
+    }
+    if(numWrong==7){
+        ctx.fillRect(138, 90, 24, 24); //cover face
+        ctx.fillRect(118, 121.2, 70, 120); //cover body
+        ctx.beginPath(); //straight mouth
+            ctx.moveTo(140,108);
+            ctx.lineTo(160,108);
+            ctx.stroke();
+        ctx.beginPath(); //body
+            ctx.moveTo(150,135);
+            ctx.lineTo(150,205);
+            ctx.stroke();
+        ctx.beginPath(); //right arm
+            ctx.moveTo(150,150);
+            ctx.lineTo(180,175);
+            ctx.stroke();
+        ctx.beginPath(); //left arm
+            ctx.moveTo(150,150);
+            ctx.lineTo(120,175);
+            ctx.stroke();
+        ctx.beginPath(); //right leg
+            ctx.moveTo(149,203);
+            ctx.lineTo(180,245);
+            ctx.stroke();
+        ctx.beginPath(); //left leg
+            ctx.moveTo(151,203);
+            ctx.lineTo(120,245);
+            ctx.stroke();
+        ctx.lineWidth=2;
+        ctx.beginPath(); //left eye
+            ctx.moveTo(140,93);
+            ctx.lineTo(146,98);
+            ctx.stroke();
+            ctx.moveTo(140,98);
+            ctx.lineTo(146,93);
+            ctx.stroke();
+        ctx.beginPath(); //right eye
+            ctx.moveTo(154,98);
+            ctx.lineTo(160,93);
+            ctx.stroke(); 
+            ctx.moveTo(154,93);
+            ctx.lineTo(160,98);
+            ctx.stroke();
+    }
+}
+
+function reset(){
+    var ul1 = document.getElementById('underline1').offsetWidth;
+    var results = document.getElementById('results');
+    var again = document.getElementById('again');
+    for(a = 1; a < 101; a++){
+        document.getElementById('letter'+a).innerHTML = "&nbsp;";
+        document.getElementById('underline'+a).style.width = ul1 + "px";
+        if(ul1 == 50){
+            document.getElementById('underline'+a).style.marginRight = "5px";
+            results.style.height = "70px";
+        }
+        else if(ul1 == 28){
+            document.getElementById('underline'+a).style.marginRight = "3px";
+            results.style.height = "50px";
+        }
+        else{
+            document.getElementById('underline'+a).style.marginRight = "3px";
+            results.style.height = "40px";
+        }
+        document.getElementById('underline'+a).style.display = "none";
+        document.getElementById('underline'+a).style.borderBottom = "0px";
+    }
+    var bank = document.getElementById("letterBank").querySelectorAll("div");
+    var cBank = document.getElementById("challengeBank").querySelectorAll("div");
+    for(b = 0; b < 26; b++){
+        bank[b].style.visibility = "visible";
+        cBank[b].style.visibility = "visible";
+    }
+    numWrong = 0;
+    numRight = 0;
+    wordLength = 0;
+    numChar = 0;
+    results.style.marginTop = "5px";
+    results.style.lineHeight = "40px";
+    results.innerHTML = " ";
+    document.getElementById('letterBank').style.display = "block";
+    again.style.marginTop = "0px";
+    again.style.display = "none";
+    document.getElementById('home').style.display = "none";
+    if(tempWords.indexOf(word) > -1){
+        tempWords.splice(rand,1);
+        Words();
+    }
+    else if(document.getElementById('charcount').innerHTML > 0){
+        document.getElementById('gamePage').style.display = "none";
+        document.getElementById('input').value = "";
+        document.getElementById('charcount').innerHTML = "0";
+    }
+}
+
+function PlayAgain(){
+    document.getElementById('gamePage').style.display = "none";
+    document.getElementById('home1').style.display = "block";
+}
